2ce4f980487d9915c2d31f3c5be574f2488d4010
YatsLilProjects <byatisha@gmail.com>
2023-11-01 11:17:19 +0530

    Added SOP

--- src/main/java/com/squidgame/SquidGameApplication.java
+++ src/main/java/com/squidgame/SquidGameApplication.java
@@ -9,6 +9,7 @@ public class SquidGameApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SquidGameApplication.class, args);
 		System.out.println("SquidGame is Running...");
+		System.out.println("Hello");
 	}
 
 }
